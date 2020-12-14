f27f70edde87a15621d9ebc50153cffbb7a60140
feldjoshuanoah <feld.joshuanoah@gmail.com>
2020-12-14 11:34:15 +0100

    Rename variables for input and output

--- Einfuehrung in die Programmierung/Hausaufgaben/02/main.cpp
+++ Einfuehrung in die Programmierung/Hausaufgaben/02/main.cpp
@@ -7,20 +7,20 @@
 using namespace std;
 
 int main() {
-    ifstream input("matrix.txt"); // input file
-    ofstream output("matrix_abs.txt"); // output file
+    ifstream in("matrix.txt"); // input file
+    ofstream out("matrix_abs.txt"); // output file
     int m = 0, n = 0; // m is the amount of rows, n the amount of columns
-    input >> m; // read m from the input file
-    input >> n; // read n from the input file
+    in >> m; // read m from the input file
+    in >> n; // read n from the input file
     for (; m > 0; m--) {
         vector<int> x(n, 0); // vector representing the current row
         for (int i = 0; i < n; i++) {
-            input >> x[i]; // reads the entries of the row vector
+            in >> x[i]; // reads the entries of the row vector
         }
         for (int i = 0; i < n; i++) {
             // writes the absolute value of each vector entry to the output
-            output << abs(x[i]) << " ";
+            out << abs(x[i]) << " ";
         }
-        output << "\n"; // adds a new line for the next row
+        out << "\n"; // adds a new line for the next row
     }
 }
